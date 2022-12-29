dd94717bc742ba7f33320ed3477aafa4f0aa4bbb
Alex <didyshen.oleksandr@gmail.com>
2022-12-29 12:50:46 +0200

    Add eplanation what to do if there are no collections

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ import './styles/normalize.css';
 function App() {
   // localStorage.clear();
   const collections = useSelector((state) => state.collections);
-  const { isCreateNewCollection } = collections;
+  const { isCreateNewCollection, collectionID } = collections;
 
   return (
     <>
       <Sidebar />
       <MainWrapper>
-        <Content>
-          <Header />
-          <TasksList />
-        </Content>
+        {collectionID === '' ? (
+          <Instruction>Select a collection to start planning</Instruction>
+        ) : (
+          <Content>
+            <Header />
+            <TasksList />
+          </Content>
+        )}
       </MainWrapper>
       {isCreateNewCollection && <CollectionModal />}
     </>
@@ -28,14 +32,23 @@ function App() {
 const MainWrapper = styled.main`
   flex: 1;
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: center;
   padding-top: 30px;
   overflow: scroll;
 `;
 
 const Content = styled.div`
+  height: 100%;
+  width: 100%;
   width: min(590px, 66vw);
+`;
+
+const Instruction = styled.div`
+  padding: 5px 15px;
+  background-color: rgb(45, 45, 45);
+  color: rgb(130, 130, 130);
+  border-radius: 15px;
 `;
 
 export default App;
